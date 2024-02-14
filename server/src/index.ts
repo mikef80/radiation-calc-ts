d754b4f5ac55b05306e6d7d98b5a86b19dbf7cb8
Mike <mike@mike-francis.org>
2024-02-14 21:09:51 +0000

    axios completed. next up is redirecting after login and restricting routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ const app = express();
 const { PORT = 8000, CLIENT_URL } = require("./constants");
 const cookieParser = require("cookie-parser");
 const passport2 = require("passport");
+const cors = require("cors");
 
 // import passport mniddleware
 require("./middlewares/passport-middleware");
@@ -10,13 +11,14 @@ require("./middlewares/passport-middleware");
 // initialised middlewares
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(passport2.initialize());
+console.log(process.env.CLIENT_URL);
 
 // import routes
 const authRoutes = require("./routes/auth");
 
 // initialise routes
 app.use("/api", authRoutes);
-
 
 module.exports = app;
