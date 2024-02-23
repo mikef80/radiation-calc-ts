d0265e4e226e50fbf0e00b22bc509440bc4d5bb7
Mike <mike@mike-francis.org>
2024-02-23 13:01:17 +0000

    still working on private routing. have implemented an auth loader for the dashboard route, but need to work out how to actually get valid auth, ratehr than hardcoded value I have

---
+++ server/src/db/data/test-data/calculations.ts
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    user_id: 1,
+    calc_type: 'RDC',
+    current_doserate: 1,
+    current_distance: 1,
+    new_distance: 2,
+    new_doserate:0.25
+  },
+  {
+    user_id: 1,
+    calc_type: 'RDC',
+    current_doserate: 1,
+    current_distance: 1,
+    new_distance: 2,
+    new_doserate:0.25
+  },
+  {
+    user_id: 2,
+    calc_type: 'RDC',
+    current_doserate: 1,
+    current_distance: 1,
+    new_distance: 2,
+    new_doserate:0.25
+  },
+]
--- server/src/db/seeds/seed.ts
+++ server/src/db/seeds/seed.ts
@@ -2,23 +2,119 @@ const format = require("pg-format");
 const db = require("../index.ts");
 const { hash } = require("bcryptjs");
 
-const seed = ({ userData }) => {
-  return db
-    .query(`DROP TABLE IF EXISTS users`)
-    .then(() => {
-      const usersTablePromise = db.query(`
-      CREATE TABLE users (
-        user_id SERIAL NOT NULL,
-        firstname VARCHAR NOT NULL,
-        lastname VARCHAR NOT NULL,
-        email VARCHAR PRIMARY KEY,
-        password VARCHAR NOT NULL
-    )`);
+const seed = ({ userData, calculationsData }) => {
+  return (
+    db
+      .query(`DROP TABLE IF EXISTS calculations;`)
+      .then(() => {
+        console.log("1");
 
-      return Promise.all([usersTablePromise]);
-    })
-    .then(() => {
-      const mappedData = userData.map(async ({ firstname, lastname, email, password }) => {
+        return db.query(`DROP TABLE IF EXISTS users;`);
+      })
+      .then(() => {
+        console.log("2");
+        const usersTablePromise = db.query(`
+          CREATE TABLE users (
+            user_id SERIAL UNIQUE NOT NULL,
+            firstname VARCHAR NOT NULL,
+            lastname VARCHAR NOT NULL,
+            email VARCHAR PRIMARY KEY,
+            password VARCHAR NOT NULL
+        );`);
+
+        console.log("3");
+
+        const calculationsTablePromise = db.query(`
+          CREATE TABLE calculations (
+            calculation_id SERIAL PRIMARY KEY NOT NULL,
+            user_id SERIAL NOT NULL REFERENCES users(user_id),
+            calc_type VARCHAR NOT NULL,
+            current_doserate DECIMAL,
+            current_distance DECIMAL,
+            new_distance DECIMAL,
+            new_doserate DECIMAL
+        );`);
+        console.log("3.5");
+
+        return Promise.all([usersTablePromise, calculationsTablePromise]);
+      })
+      /* .then(() => {
+      console.log("3");
+      const calculationsTablePromise = db.query(`
+      CREATE TABLE calculations (
+        calculation_id SERIAL PRIMARY KEY NOT NULL,
+        user_id SERIAL NOT NULL REFERENCES users(user_id),
+        calc_type VARCHAR NOT NULL,
+        current_doserate DECIMAL,
+        current_distance DECIMAL,
+        new_distance DECIMAL,
+        new_doserate DECIMAL
+    );`);
+      console.log("3.5");
+
+      return Promise.all([calculationsTablePromise]);
+    }) */
+      .then(async () => {
+        const mappedUserData = await userData.map(
+          async ({ firstname, lastname, email, password }) => {
+            const hashedPassword = await new Promise((resolve, reject) => {
+              hash(password, 10, (err, hash) => {
+                if (err) reject(err);
+                resolve(hash);
+              });
+            });
+
+            return { firstname, lastname, email, hashedPassword };
+          }
+        );
+
+        console.log("4");
+        return Promise.all(mappedUserData);
+      })
+      .then((mappedUserData) => {
+        const insertUsersQueryStr = format(
+          "INSERT INTO users (firstname, lastname, email, password) VALUES %L;",
+          mappedUserData.map(({ firstname, lastname, email, hashedPassword }) => [
+            firstname,
+            lastname,
+            email,
+            hashedPassword,
+          ])
+        );
+
+        const usersPromise = db.query(insertUsersQueryStr);
+
+        const insertCalcQueryStr = format(
+          "INSERT INTO calculations (user_id, calc_type, current_doserate, current_distance, new_distance, new_doserate) VALUES %L;",
+          calculationsData.map(
+            ({
+              user_id,
+              calc_type,
+              current_doserate,
+              current_distance,
+              new_distance,
+              new_doserate,
+            }) => [
+              user_id,
+              calc_type,
+              current_doserate,
+              current_distance,
+              new_distance,
+              new_doserate,
+            ]
+          )
+        );
+
+        const calcsPromise = db.query(insertCalcQueryStr);
+
+        console.log("5");
+        return Promise.all([usersPromise, calcsPromise]);
+      })
+  );
+
+  /* .then(async () => {
+      console.log("4");
+      const mappedUserData = await userData.map(async ({ firstname, lastname, email, password }) => {
         const hashedPassword = await new Promise((resolve, reject) => {
           hash(password, 10, (err, hash) => {
             if (err) reject(err);
@@ -29,17 +125,30 @@ const seed = ({ userData }) => {
         return [firstname, lastname, email, hashedPassword];
       });
 
-      return Promise.all(mappedData);
-    })
-    .then((mappedData) => {
+      console.log(mappedUserData,'<--***');
+      
+
+      return Promise.all([mappedUserData]);
+    }) */
+  /* .then((mappedUserData) => {
+      console.log("5");
+      console.log(mappedUserData);
+
       const insertUsersQueryStr = format(
         "INSERT INTO users (firstname, lastname, email, password) VALUES %L;",
-        mappedData
+        mappedUserData
       );
       const usersPromise = db.query(insertUsersQueryStr);
 
-      return Promise.all([usersPromise]);
-    });
+      const insertCalculationsQueryStr = format(
+        "INSERT INTO calculations (user_id, calc_type, current_doserate, current_distance, new_distance, new_doserate) VALUES %L;",
+        calculationsData
+      );
+
+      const calculationsPromise = db.query(insertCalculationsQueryStr);
+
+      return Promise.all([usersPromise, calculationsPromise]);
+    }); */
 };
 
 module.exports = seed;
