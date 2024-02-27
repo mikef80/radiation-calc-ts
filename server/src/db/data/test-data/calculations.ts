ee1192838c758b22e792c9cd91b6daf3954711cc
Mike <mike@mike-francis.org>
2024-02-27 22:08:09 +0000

    rebuilding calculation details

--- server/src/db/data/test-data/calculations.ts
+++ server/src/db/data/test-data/calculations.ts
@@ -1,29 +1,35 @@
 module.exports = [
   {
-    calculation_date: '2024-01-01',
+    calculation_date_time: new Date("2024-01-01"),
     user_id: 1,
-    calculation_type: 'RDC',
+    calculation_type: "RDC",
     current_doserate: 1,
     current_distance: 1,
-    new_distance: 2,
-    new_doserate:0.25
+    new_operating_distance: 2,
+    new_doserate: 0.25,
+    calculation_unit: "µSv/hr",
+    distance_unit: "metres",
   },
   {
-    calculation_date: '2024-02-01',
+    calculation_date_time: new Date("2024-01-01"),
     user_id: 1,
-    calculation_type: 'RDC',
+    calculation_type: "RDC",
     current_doserate: 1,
     current_distance: 1,
-    new_distance: 2,
-    new_doserate:0.25
+    new_operating_distance: 2,
+    new_doserate: 0.25,
+    calculation_unit: "µSv/hr",
+    distance_unit: "metres",
   },
   {
-    calculation_date: '2024-02-01',
+    calculation_date_time: new Date("2024-01-01"),
     user_id: 2,
-    calculation_type: 'RDC',
+    calculation_type: "RDC",
     current_doserate: 1,
     current_distance: 1,
-    new_distance: 2,
-    new_doserate:0.25
+    new_operating_distance: 2,
+    new_doserate: 0.25,
+    calculation_unit: "µSv/hr",
+    distance_unit: "metres",
   },
-]
+];
--- server/src/routes/data.ts
+++ server/src/routes/data.ts
@@ -1,9 +1,10 @@
 import { Router as DataRouter } from "express";
-const { getCalcs } = require("../controllers/data");
+const { getCalcs, postCalc } = require("../controllers/data");
 const { userAuth } = require("../middlewares/auth-middleware");
 const dataRouter = DataRouter();
 
 // data routes
 dataRouter.get("/calcs", userAuth, getCalcs);
+dataRouter.post("/calcs", userAuth, postCalc);
 
 module.exports = dataRouter;
