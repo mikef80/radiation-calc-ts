d7f4005522b1e443010476dc2a604e988e8ff45d
Mike <mike@mike-francis.org>
2024-03-12 22:12:04 +0000

    pushing before seeding PROD

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -10,12 +10,12 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
-/* const config: { [k: string]: any } = {};
+const config: { [k: string]: any } = {};
 
 if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
-} */
+}
 
 const config2 = {};
 
