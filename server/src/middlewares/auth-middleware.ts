256f41201f31f3b0fe11a6f7a48ae1de3b34e657
Mike <mike@mike-francis.org>
2024-03-13 13:31:41 +0000

    updated endpoints definitions

--- server/src/middlewares/auth-middleware.ts
+++ server/src/middlewares/auth-middleware.ts
@@ -3,5 +3,8 @@ import { NextFunction, Request, Response } from "express";
 const passportAuth = require("passport");
 
 exports.userAuth = (req: Request, res: Response, next: NextFunction) => {
+  console.log('passportAuth1');
+  
   passportAuth.authenticate("jwt", { session: false })(req, res, next);
+  console.log('passportAuth2');
 };
--- server/src/routes/data.ts
+++ server/src/routes/data.ts
@@ -1,11 +1,11 @@
 import { Router as DataRouter } from "express";
-const { getCalcs, postCalc,getEndpoints } = require("../api/controllers/data-controllers");
+const { getCalcs, postCalc, getEndpoints } = require("../api/controllers/data-controllers");
 const { userAuth } = require("../middlewares/auth-middleware");
 const dataRouter = DataRouter();
 
 // data routes
 dataRouter.get("/calcs", userAuth, getCalcs);
 dataRouter.post("/calcs", userAuth, postCalc);
-dataRouter.get('/', getEndpoints)
+dataRouter.get("/",  getEndpoints);
 
 module.exports = dataRouter;
