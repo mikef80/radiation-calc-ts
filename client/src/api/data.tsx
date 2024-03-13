68425e6185b5f99d9ba01ad0b90a5b7fc685ad2a
Mike <mike@mike-francis.org>
2024-03-13 12:40:17 +0000

    updated cors

--- client/src/api/data.tsx
+++ client/src/api/data.tsx
@@ -1,10 +1,13 @@
 import axios from "axios";
 axios.defaults.withCredentials = true;
 
+// const serverURL = "http://localhost:8000";
+const serverURL = 'https://radiation-calc-ts.onrender.com'
+
 export async function getCalculations() {
-  return await axios.get("http://localhost:8000/data/calcs");
+  return await axios.get(`${serverURL}/data/calcs`);
 }
 
 export async function postCalculation(calcData: any) {
-  return await axios.post("http://localhost:8000/data/calcs", calcData);
+  return await axios.post(`${serverURL}/data/calcs`, calcData);
 }
