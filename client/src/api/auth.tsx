d13be9ba2edf5271635fe75ba3be8a243192f65a
Mike <mike@mike-francis.org>
2024-03-13 19:55:24 +0000

    updated endpoints definitions

--- client/src/api/auth.tsx
+++ client/src/api/auth.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 // const serverURL = "http://localhost:8000";
-const serverURL = 'https://radiation-calc-ts.onrender.com'
+const serverURL = "https://radiation-calc-ts.onrender.com";
 
 export async function onRegistration(registrationData: any) {
   return await axios.post(`${serverURL}/api/register`, registrationData);
