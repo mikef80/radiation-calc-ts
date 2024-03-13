7d9b096e0ea0d6f8f671079c30381931adce5ce2
Mike <mike@mike-francis.org>
2024-03-13 15:28:26 +0000

    updated endpoints definitions

--- server/src/middlewares/auth-middleware.ts
+++ server/src/middlewares/auth-middleware.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express";
 
 const passportAuth = require("passport");
 
-exports.userAuth = (req: Request, res: Response, next: NextFunction) => {
+/* exports.userAuth = (req: Request, res: Response, next: NextFunction) => {
   passportAuth.authenticate("jwt", { session: false }, (err, user, info) => {
     if (err) {
       console.log(err);
@@ -16,7 +16,9 @@ exports.userAuth = (req: Request, res: Response, next: NextFunction) => {
     console.log("all fine");
     return res.status(200).json({ message: "Auth Passed" });
   })(req, res, next);
-};
-/* exports.userAuth = (req: Request, res: Response, next: NextFunction) => {
-  passportAuth.authenticate("jwt", { session: false })(req, res, next);
 }; */
+
+
+exports.userAuth = (req: Request, res: Response, next: NextFunction) => {
+  passportAuth.authenticate("jwt", { session: true })(req, res, next);
+};
