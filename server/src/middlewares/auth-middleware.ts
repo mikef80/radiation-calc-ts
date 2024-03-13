d889dacb4fc8925ad5f157a4057d04fd2c1be994
Mike <mike@mike-francis.org>
2024-03-13 22:56:52 +0000

    trying to figure out if user details not being added to protected routes

--- server/src/middlewares/auth-middleware.ts
+++ server/src/middlewares/auth-middleware.ts
@@ -5,6 +5,8 @@ const passportAuth = require("passport");
 exports.userAuth = (req: Request, res: Response, next: NextFunction) => {
   passportAuth.authenticate("jwt", { session: false }, (err, user, info) => {
     if (!user) {
+      console.log(err, "<==err");
+
       console.log("NO USER!");
     }
   })(req, res, next);
